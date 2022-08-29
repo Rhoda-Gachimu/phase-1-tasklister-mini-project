7b609499dd956c76043177af1773c3096dddc101
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-08-29 22:52:00 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  const ul = document.getElementById('tasks')
+  const inputDescription = document.getElementById('new-task-description')
+
+  function addTask() {
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'Delete'
+    li.textContent = `${inputDescription.value}  `
+    ul.appendChild(li)
+    li.appendChild(btn)
+  }
+
+  function handleDelete(event) {
+    event.target.parentNode.remove()
+  }
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    addTask()
+    form.reset()
+
+  })
 });
